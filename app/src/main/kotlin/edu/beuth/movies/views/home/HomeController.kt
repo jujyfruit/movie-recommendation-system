c10cd87364c4130241d191d2adccf734743de90d
itrajanovska <s76633@beuth-hochschule.de>
2019-01-18 16:23:10 +0100

    Add a page through which users can select reference movies and receive recommendations

--- app/src/main/kotlin/edu/beuth/movies/views/home/HomeController.kt
+++ app/src/main/kotlin/edu/beuth/movies/views/home/HomeController.kt
@@ -6,7 +6,6 @@ import edu.beuth.movies.services.recommender.MovieRecommender
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
@@ -16,9 +15,8 @@ import org.springframework.web.bind.annotation.ResponseBody
 class HomeController(private val movieRecommender: MovieRecommender,
                      private val movieService: MovieService) {
 
-    @GetMapping("/home")
-    fun greeting(@RequestParam(name = "name", required = false, defaultValue = "World") name: String, model: Model): String {
-        model.addAttribute("name", movieRecommender.getMovieRecommendations(listOf()).joinToString())
+    @GetMapping("/")
+    fun home(): String {
         return "home"
     }
 
@@ -26,5 +24,11 @@ class HomeController(private val movieRecommender: MovieRecommender,
     @ResponseBody
     fun movies(@RequestParam(name = "searchTerm") searchTerm: String, pageable: Pageable): Page<Movie> {
         return movieService.findMovies(searchTerm, pageable)
+    }
+
+    @GetMapping("/api/getRecommendations")
+    @ResponseBody
+    fun getRecommendations(@RequestParam(name = "referenceMovies[]") referenceMovies: List<String>): List<String> {
+        return movieRecommender.getMovieRecommendations(referenceMovies)
     }
 }
--- app/src/main/resources/static/js/home.js
+++ app/src/main/resources/static/js/home.js
@@ -7,15 +7,16 @@ $(document).ready(function () {
             url: "/api/movies",
             dataType: 'json',
             delay: 250,
+            width: 'resolve',
             data: function (params) {
                 return {
                     searchTerm: params.term || '', // search term
-                    page: params.page || 1,
+                    page: params.page || 0,
                     size: pageSize
                 };
             },
             processResults: function (data, params) {
-                params.page = params.page || 1;
+                params.page = params.page || 0;
 
                 return {
                     results: data.content.map(function (it) {
@@ -28,6 +29,30 @@ $(document).ready(function () {
             },
             cache: true
         },
-        placeholder: 'Search for a repository'
+        placeholder: 'Search for a movie'
     });
+
+    $('#get-recommendations').click(function () {
+        $.ajax({
+            url: '/api/getRecommendations',
+            data: {
+                referenceMovies: $('#select-container').select2('data').map(function (it) {
+                    return it.text;
+                })
+            },
+            success: function (recommendedMovies) {
+                displayRecommendedMovies(recommendedMovies);
+            }
+        })
+    });
+
+    function displayRecommendedMovies(recommendedMovies) {
+        var $recommendations = $('#recommendations');
+        $recommendations.empty();
+
+        for (var i = 0; i < recommendedMovies.length; i++) {
+            var $div = $("<div>", {text: recommendedMovies[i]});
+            $recommendations.append($div);
+        }
+    }
 });
