70fb0be0d22538d5e90316ce5f538262e72e9a42
itrajanovska <s76633@beuth-hochschule.de>
2019-02-07 15:04:33 +0100

    Address sonarqube code smells

--- app/src/test/java/edu/beuth/movies/MovieApplicationTests.java
+++ app/src/test/java/edu/beuth/movies/MovieApplicationTests.java
@@ -1,16 +1,16 @@
 package edu.beuth.movies;
 
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = edu.beuth.movies.services.recommender.pb.MovieRecommenderGrpc.MovieRecommenderBlockingStub.class)
-public class MoviesApplicationTests {
+@SpringBootTest(classes = {MoviesApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class MovieApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        // tries to load the application
     }
 }
